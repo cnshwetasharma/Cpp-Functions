27152e31357f79e2c8bc74eb13690193bd9a0b61
cnshwetasharma <devshwetasharma07@gmail.com>
2025-12-16 20:55:40 +0530

    Add :: flowchat code4.cpp

---
+++ 07.Flowchart/code4.cpp
@@ -0,0 +1,13 @@
+// Even and Odd by using terinary operator  
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num;
+    cin>>num;
+
+    cout<<(num%2 == 0 ? "Even" :"Odd");
+    
+    return 0;
+}
