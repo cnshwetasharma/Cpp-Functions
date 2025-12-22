96d1b717330399e02d99bb1cc7fd891c4ef8c9ee
cnshwetasharma <devshwetasharma07@gmail.com>
2025-12-22 22:45:43 +0530

    Add :: file added code.cpp

---
+++ 09.Operator/code.cpp
@@ -0,0 +1,13 @@
+// side of sqaure 
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num;
+    
+    cout<< "Enter a number : " , cin>>num;
+    cout<<"Area of the Square : "<<num*num;
+    
+    return 0;
+}
